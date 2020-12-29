bd7102b73da50e010afaae0a97442e7820da0db4
jaysbayona <60095082+jaysbayona@users.noreply.github.com>
2020-12-29 10:10:00 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		
+		String jaysBayona = "Jason Bayona";
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println(jaysBayona + "Yow");
+		
+
+	}
+
+}
+
+
+
